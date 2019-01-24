3b7835dcee9f6f22ddff0548435bfa4ccbb84416
carlos <huaixian.huang@gmail.com>
2019-01-24 19:53:09 +0800

    feat: switch between component

--- src/App.js
+++ src/App.js
@@ -4,25 +4,38 @@ import lodash from "lodash";
 import { Grid, Row, Col } from "react-flexbox-grid";
 import { StyleSheet, css } from "aphrodite";
 import { Modal, Button, Divider, List, Avatar, Input } from "antd";
+import { BrowserRouter as Router, Route, Link,Redirect,Switch} from "react-router-dom";
 import { queue } from "async";
+import VirtualList from "react-virtual-list";
 
 import axios from "axios";
 
 import logo from "./logo.svg";
 import "./App.css";
-import VirtualList from "react-virtual-list";
 
-// import * as token from './token.js';
-// const { ipcRenderer } = window.require('electron');
 
 var { ipcRenderer } = window.require("electron");
+const Index = () => <h2>Home<Button onClick={()=>{ if(global_this){global_this.setState({ redirectToReferrer: true })}}}>Hicarlos</Button></h2>;
+const About = () => <h2>About</h2>;
+const Users = () => <h2>Users</h2>;
+
+var global_this;
+
 
 class App extends Component {
+  state = { cur_path: "/Users" };
   render() {
-    return (
-      <div className="App">
-        <a>hello world</a>
-      </div>
+    global_this = this;
+    let { redirectToReferrer } = this.state;
+    if (redirectToReferrer) return <Redirect to={'/About'} />;
+    return(
+      <Router>
+        <Switch location={{pathname:this.state.cur_path}}>
+          <Route path="/about" component={About} />
+          <Route path="/users" component={Users} />
+          <Route path="/" exact component={Index} />
+        </Switch>
+      </Router>
     );
   }
 }
