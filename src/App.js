6b423a592b0a90571be9450ce77f14e211c149b8
carlos <huaixian.huang@gmail.com>
2019-01-24 20:30:12 +0800

    feat: add redux and react-redux

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { StyleSheet, css } from "aphrodite";
 import { Modal, Button, Divider, List, Avatar, Input } from "antd";
 import { BrowserRouter as Router, Route, Link,Redirect,Switch} from "react-router-dom";
 import { queue } from "async";
+import { Provider } from 'react-redux'
+
 import VirtualList from "react-virtual-list";
 
 import axios from "axios";
@@ -15,7 +17,7 @@ import "./App.css";
 
 
 var { ipcRenderer } = window.require("electron");
-const Index = () => <h2>Home<Button onClick={()=>{ if(global_this){global_this.setState({ redirectToReferrer: true })}}}>Hicarlos</Button></h2>;
+const Index = () => <h2>Home<Button onClick={()=>{ if(global_this){global_this.setState({ cur_path: "/about"})}}}>Hicarlos</Button></h2>;
 const About = () => <h2>About</h2>;
 const Users = () => <h2>Users</h2>;
 
@@ -23,16 +25,15 @@ var global_this;
 
 
 class App extends Component {
-  state = { cur_path: "/Users" };
+  state = { cur_path: "/" };
   render() {
     global_this = this;
     let { redirectToReferrer } = this.state;
-    if (redirectToReferrer) return <Redirect to={'/About'} />;
     return(
       <Router>
         <Switch location={{pathname:this.state.cur_path}}>
           <Route path="/about" component={About} />
-          <Route path="/users" component={Users} />
+          <Route path="/abouts" component={Users} />
           <Route path="/" exact component={Index} />
         </Switch>
       </Router>
---
+++ src/reducers.js
@@ -0,0 +1,5 @@
+export default function todoApp(state = {}, action) {
+  // For now, don't handle any actions
+  // and just return the state given to us.
+  return state
+}
