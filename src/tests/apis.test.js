64eb3d375925226f24a8f5a5b8d93600a93986b2
carlos <huaixian.huang@gmail.com>
2019-01-28 10:20:02 +0800

    feat: add gitlab api and test code

---
+++ src/tests/apis.test.js
@@ -0,0 +1,18 @@
+var gitlabapi = require('../gitlabs/apis.js');
+var test_data = null;
+var test_token = "";
+try{
+  test_data = require('../test_data/gitlab.json');
+  test_token = test_data.token;
+}
+catch(e){
+  console.log('require json e:',e);
+}
+
+
+test('Login api ', async () => {
+  var gitlabpaiInstance = new gitlabapi();
+  var ret = await gitlabpaiInstance.initInstance(test_token);
+  // console.log('ret is:',ret);
+  expect(200).toBe(ret.status);
+});
--- src/tests/lejuhubapis.js
+++ src/tests/lejuhubapis.js
@@ -1,12 +1,15 @@
 var gitlabapi = require('../gitlabs/apis.js');
 var test_data = null;
+var test_token = "";
 try{
   test_data = require('../test_data/gitlab.json');
+  test_token = test_data.token;
 }
 catch(e){
+  console.log('require json e:',e);
 }
 
-console.log('test data is:',test_data);
+console.log('test data is:',test_data,test_token);
 
 var test = {};
 
